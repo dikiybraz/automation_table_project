2d0a2595ca6bd04c0301ed71c1402608dbf6ce7e
dikiybraz <fadeev2012@gmail.com>
2020-11-27 01:06:59 +0300

    First upload from PyCharm!

---
+++ scratch.py
@@ -0,0 +1,48 @@
+class table:
+    def turn_cw(self):
+        print('Table was turned cw')
+
+    def turn_ccw(self):
+        print('Table was turned ccw')
+
+
+
+    pass
+
+class piezo_motor_x: #поворот столика по часовой стрелке
+    def __init__(self):
+        self.x=0
+    def turn_cw(self):
+        self.x+=1
+    def turn_ccw(self):
+        self.x-=1
+    pass
+class piezo_motor_y: #поворот столика против часовой стрелки
+    def __init__(self):
+        self.y=0
+    def turn_cw(self):
+        self.y+=1
+    def turn_ccw(self):
+        self.y-=1
+class chip():
+
+    def progressbar(self):
+        pass
+
+    pass
+
+class optic_power_meter:
+
+    def __init__(self):
+        self.meter=0
+    def progressbar(self):
+        for self.meter in 100:
+            self.meter+=1
+        pass
+
+    pass
+#class connected_chip: pass
+class visualization: #иллюстрация активности
+    pass
+class light_intensivity:
+    pass
