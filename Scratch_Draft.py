fdc1d351246fb8a2ff4e9e6fe0af59bc78fbfbd1
dikiybraz <fadeev2012@gmail.com>
2021-02-10 16:22:50 +0300

    new text + edited draft

--- Scratch_Draft.py
+++ Scratch_Draft.py
@@ -2,64 +2,48 @@ from numpy.ma import sqrt
 from math import acos
 from math import radians
 import numpy as np
-
-
 class Experimental_Setup:
     def __init__(self, table, env):
         self.table = table
         self.env = env
 
     def move_table(self, dx, dy):
-        self.table.move(dx, dy)
+        self.env.move(dx, dy)
 
     def measure(self):
         return self.env.measure()
 
+class Environment:
+    def __init__(self, table, chip):
+        self.table = table
+        self.chip = chip
+
+    def move(self, dx, dy):
+        self.table.move(dx, dy)
+
+    def measure(self):
+        return self.chip.value(self.table.x, self.table.y)
+
 
 class Table:
-    def __init__(self, motor_x, motor_y):
+    def __init__(self, motor_x, motor_y , x , y):
         self.motor_x = motor_x
         self.motor_y = motor_y
+        self.x = x
+        self.y = y
 
-    def move(self, x, y):
-        # шаг шарико-винтовой передачи Ph =1 [мм], среднее минимальное линейное перемещение l (х и y) можно
-        # определить по формуле
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
 
-        dx = (0.1 * self.motor_x.rotate(self.motor_x.angle)) / (2 * 3.14)
-        dy = (0.1 * self.motor_y.rotate(self.motor_y.angle)) / (2 * 3.14)
-
-        x = x + dx
-        y = y + dy
-
-        return [x, y]
-
-    def get_coords(self):
-        # ???
-        pass
-
-    pass
 
 
 class MockMotor:
     def __init__(self, angle):
         self.angle = angle
 
-    def rotate(self, angle):
-        angle = angle + 0.01
-        return angle
-
-
-class Environment:
-    def __init__(self, mocktable, chip):
-        self.table = mocktable
-        self.chip = chip
-
-    def move(self, dx, dy):
-        return self.table.move(dx, dy)
-
-    def measure(self):
-        return self.chip.value(self.table.x, self.table.y)
-
+    def rotate(self):
+        return self.angle + 1
 
 class Chip:
     def __init__(self, chip_array):
@@ -73,34 +57,16 @@ class Chip:
         return self.chip_array[x, y]
 
 
-arr = np.array([[0, 3, 4, 0, 2],
-                [5, 0, 0, 7, 3],
-                [0, 4, 6, 9, 0],
-                [0, 4, 5, 0, 2],
-                [1, 0, 5, 4, 3]])
+arr = np.array([])
 # массив 5х5 - плоскость с разными оптическими мощностями
-motor_x = MockMotor(0)
-motor_y = MockMotor(1)
+
+motor_x = MockMotor(3)
+motor_y = MockMotor(4)
 chip = Chip(arr)
-table = Table(motor_x, motor_y)
+table = Table(motor_x, motor_y, 1, 1)
 env = Environment(table, chip)
 ex_setup = Experimental_Setup(table, env)
 
-# проверка класс environment
-# print(ex_setup.measure())  #done
-# print(ex_setup.move_table())
-
-# проверка класс environment
-# print(env.move(4, 3)) # Done
-# print(env.measure())
-
-# проверка класса Table
-# print(table.move(2, 1)) # done
-
-# проверка класса чип
-# print(chip.value(1, 3)) # done
-# print(chip.screen()) # done
-
-# проверка класса мотор
-# print(motor_x.rotate(0.11)) # done
-
+print(ex_setup.measure())
+print(ex_setup.move_table(1, 1))
+print(ex_setup.measure())
